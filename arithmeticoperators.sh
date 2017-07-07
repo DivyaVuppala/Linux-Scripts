53404cb384352b2197a5b065deb06d6632d762c0
DivyaVuppala <divya.vuppala0603@gmail.com>
2017-07-07 15:40:44 -0700

    2 new files

---
+++ arithmeticoperators.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# performing arithmetic operations
+
+echo "------Addition------"
+expr 2 + 2
+
+echo "-----Subtraction-----"
+expr 5 - 3
+
+echo "----Multiplication----"
+expr 10 \* 4
+
+echo "----Division-------"
+expr 6 / 3 
+
+echo "----Modulus-------"
+expr 8 % 3
+
+echo "-----Expression----"
+expr 4 + 4 - 2 \* 10
+
---
+++ exitconcept.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#set -e.... To stop the execution once there occurs an error
+echo "================================="
+echo "This is a sample statement"
+echo $?
+
+echo "================================="
+cd jhfjkhdjk.txt
+echo $?
+
+echo "================================"
+expr 2 + 2
+echo $?
+
+
